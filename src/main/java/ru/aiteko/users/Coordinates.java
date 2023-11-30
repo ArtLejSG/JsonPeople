0107ea68de7361a38747552317b91363c38bf3ed
ArtLejSG <146709851+ArtLejSG@users.noreply.github.com>
2023-11-30 16:48:28 +0300

    Coordinates.java

--- src/main/java/ru/aiteko/users/Coordinates.java
+++ src/main/java/ru/aiteko/users/Coordinates.java
@@ -1,48 +1,11 @@
-//package ru.aiteko.users;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import java.util.Objects;
-//import  com.google.gson.Gson;
-//import java.util.Map;
-//@Data
-//@AllArgsConstructor
-//public class Coordinates {
-//    private double latitude;
-//    private double longtitude;
-//}
-//    private Double latitude;
-//    private Double longitude;
-//
-//   public Coordinates(Double latitude, Double longitude){
-//       this.latitude = latitude;
-//       this.longitude = longitude;
-//   }
-//
-//    public static void main(String[] args) {
-//    String jsonDouble = "{\"coordinates\": {\"latitude\": -39.0855, \"longtitude\": -36.2158}}";
-//    Gson gson = new Gson();
-//    Map<String, Map<String, Double>> data = gson.fromJson(jsonDouble, Map.class);
-//    Map<String, Double> coordinatesData = data.get("coordinates");
-//    Coordinates coordinates = new Coordinates(coordinatesData.get("latitude"), coordinatesData.get("longtitude"));
-//        System.out.println(coordinates.getLatitude());   // Giles
-//        System.out.println(coordinates.getLongtitude());  // Brooklyn
-//    }
-//
-//    public Double getLatitude() {
-//        return latitude;
-//    }
-//
-//    public Double getLongtitude() {
-//        return longitude;
-//    }
-//}
+
 package ru.aiteko.users;
 
 import java.util.Objects;
 
 public record Coordinates(double latitude, double longitude) {
     public Coordinates {
-        // Валидация или другая логика конструктора может быть добавлена здесь
+        
         if (latitude < -90 || latitude > 90 || longitude < -180 || longitude > 180) {
             throw new IllegalArgumentException("Invalid coordinates");
         }
